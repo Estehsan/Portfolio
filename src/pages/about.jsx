5069226368fbd79fb8cc80d8e49ec3910e837138
estehsan <estehsan.tariq@sas.se>
2024-02-26 23:19:03 +0100

    Updated Work Experience

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -60,7 +60,7 @@ export default function About() {
           </div>
           <div className="lg:order-first lg:row-span-2">
             <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Meet Estehsan Tariq: The Multi-Talented Marketing Maverick from Gothenburg
+            Meet Estehsan Tariq: The Multi-Talented Marketing Maverick from Stockholm
             </h1>
             <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
               <p>
